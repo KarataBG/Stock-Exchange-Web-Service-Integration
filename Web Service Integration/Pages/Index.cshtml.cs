c11a41a0f3edee86f7f87dd41c0f8cfd9a1caf7f
KarataBG <niki.karata@gmail.com>
2024-12-14 19:59:06 +0200

    prettify

--- Web Service Integration/Pages/Index.cshtml.cs
+++ Web Service Integration/Pages/Index.cshtml.cs
@@ -13,10 +13,21 @@ namespace Web_Service_Integration.Pages
         }
         public string BestPerformer { get; private set; }
         public decimal BestGrowth { get; private set; }
+
+        public Dictionary<string, decimal> GrowthData { get; private set; } = new Dictionary<string, decimal>();
+
         public void OnGet()
         {
             BestPerformer = "GOOGLE";
-            BestGrowth = 100;
+            BestGrowth = 99999999;
+
+            GrowthData.Add("1", 333);
+            GrowthData.Add("2", 25235);
+            GrowthData.Add("3", 7457487);
+            GrowthData.Add("4", 3573);
+            GrowthData.Add("5", 1230979);
+
+            Page();
         }
     }
 }
